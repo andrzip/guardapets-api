830785083f6ede54c3e2010d57e911937a0213eb
Andre <andrweb876@gmail.com>
2024-06-29 15:25:33 -0300

    #

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: process.env.POSTGRES_HOST || "localhost",
+    host: process.env.MYSQLHOST || "localhost",
     user: process.env.POSTGRES_USER || "root",
-    password: process.env.POSTGRES_PASSWORD || "root",
-    database: process.env.POSTGRES_DATABASE ||"guardapet_database",
+    password: process.env.MYSQLPASSWORD || "root",
+    database: process.env.MYSQLDATABASE ||"guardapet_database",
     port: "3306"
 });
 
@@ -13,5 +13,5 @@ db.connect((err) => {
         console.error('❌ - MYSQL Erro de Conexão:\n', err.message);
         return;
     }
-    console.log(`✅ - Database Conectada! HOST -> [${process.env.POSTGRES_HOST}]`);
+    console.log(`✅ - Database Conectada! HOST -> [${process.env.MYSQLHOST}]`);
 });
