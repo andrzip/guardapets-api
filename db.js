988acb5297c897a3d53a22d29bb1e4c6b47276fa
André <79728727+andrzip@users.noreply.github.com>
2024-08-12 13:30:38 +0000

    update

--- db.js
+++ db.js
@@ -1,16 +1,20 @@
 import mysql from "mysql2";
+import dotenv from "dotenv/config.js";
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "guardapet_database",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('❌ - MYSQL Erro de Conexão:\n', err.message);
-        return;
-    }
-    console.log(`✅ - Database Conectada! HOST -> [localhost]`);
+  if (err) {
+    console.error("❌ - MYSQL Erro de Conexão:\n", err);
+    return;
+  }
+  console.log(
+    `✅ - Database Conectada! HOST -> [${process.env.MYSQLDATABASE}]`
+  );
 });
